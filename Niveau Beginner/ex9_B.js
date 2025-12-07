6144cc1941c33d5695d87707754b664300ffcc2f
mohamed <mojahdontheway@gmail.com>
2025-12-07 17:25:39 +0100

    add esercies 9

---
+++ Niveau Beginner/ex9_B.js
@@ -0,0 +1,11 @@
+const data = [12, 5, 8, 130, 44, 9, 2, 77, 21, 56, 3, 99, 14, 28, 1, 65, 7, 38, 11, 50];
+
+let bigThen = 20;
+for(let i = 0 ; i< data.length ; i++){
+    if(bigThen < data[i]){
+      console.log(data[i]);
+    }
+
+}
+
+
