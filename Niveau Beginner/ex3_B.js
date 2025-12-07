e2d5b7f7f5e5551f3426b6ff57f06fc01879d704
mohamed <mojahdontheway@gmail.com>
2025-12-07 13:20:34 +0100

    add exercies 3 from niveau beginner

---
+++ Niveau Beginner/ex3_B.js
@@ -0,0 +1,7 @@
+const data = [12, 5, 8, 130, 44, 9, 2, 77, 21, 56, 3, 99, 14, 28, 1, 65, 7, 38, 11, 50];
+
+for(let i =0 ; i<data.length ; i++){
+    let double_num = data[i];
+    console.log(double_num * 2);
+}
+
