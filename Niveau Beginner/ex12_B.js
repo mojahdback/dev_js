63058ab90e74a6532193c142c08e72b7cb7a3ce6
mohamed <mojahdontheway@gmail.com>
2025-12-07 18:24:08 +0100

    add exercies 12

---
+++ Niveau Beginner/ex12_B.js
@@ -0,0 +1,13 @@
+const data = [12, 5, 8, 130, 44, 9, 2, 77, 21, 56, 3, 99, 14, 28, 1, 65, 7, 38, 11, 50];
+
+let count = 0;
+
+for(let i = 0; i < data.length ; i++){
+    if(data[i] > 50 ){
+        count++;
+    }
+
+}
+
+
+console.log("The numbers largest then 50 is  " + count);
