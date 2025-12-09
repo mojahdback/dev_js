f827714af4d6ba7f3612dea74c0264d731ea7b8c
mohamed <mojahdontheway@gmail.com>
2025-12-10 00:28:31 +0100

    sorting by age and score users from object

---
+++ object/obj.js
@@ -0,0 +1,55 @@
+const users = [
+  { id: 1, name: "Kamal", age: 22, city: "Marrakech", score: 45 },
+  { id: 2, name: "Sami", age: 30, city: "Rabat", score: 72 },
+  { id: 3, name: "Issam", age: 28, city: "Marrakech", score: 60 },
+  { id: 4, name: "Oualid", age: 19, city: "Agadir", score: 33 },
+  { id: 5, name: "Hassan", age: 33, city: "Casablanca", score: 80 },
+  { id: 6, name: "Farah", age: 25, city: "Marrakech", score: 50 },
+  { id: 7, name: "Salma", age: 40, city: "Rabat", score: 90 },
+  { id: 8, name: "Hanae", age: 21, city: "Marrakech", score: 41 },
+  { id: 9, name: "Ibrahim", age: 27, city: "Casablanca", score: 55 },
+  { id: 10, name: "Jade", age: 24, city: "Agadir", score: 48 }
+];
+
+// sorting by using sort(age)
+
+const sortAge = users.sort((a,b) => a.age - b.age );
+
+console.log(sortAge);
+
+// sorting by using sort(score)
+
+const sortScore = users.sort((a,b) =>  b.score - a.score );
+
+console.log(sortScore);
+
+
+// using buble sort (age)
+
+for(let i=0 ; i< users.length -1 ; i++){
+    for(let j= i+1 ; j< users.length ;j++){
+        if(users[i].age > users[j].age){
+            let temp = users[i];
+            users[i] = users[j];
+            users[j] = temp;
+        }
+    } 
+}
+
+console.log(users);
+
+// using buble sort (score)
+
+for(let i=0 ; i< users.length -1 ; i++){
+    for(let j= i+1 ; j< users.length ;j++){
+        if(users[i].score < users[j].score){
+            let temp = users[i];
+            users[i] = users[j];
+            users[j] = temp;
+        }
+    } 
+}
+
+console.log(users);
+
+
---
+++ object/test.js
@@ -0,0 +1,13 @@
+
+const users = [
+  { id: 1, name: "Kamal", age: 22, city: "Marrakech", score: 45 },
+  { id: 2, name: "Sami", age: 30, city: "Rabat", score: 72 },
+  { id: 3, name: "Issam", age: 28, city: "Marrakech", score: 60 },
+  { id: 4, name: "Oualid", age: 19, city: "Agadir", score: 33 },
+  { id: 5, name: "Hassan", age: 33, city: "Casablanca", score: 80 },
+  { id: 6, name: "Farah", age: 25, city: "Marrakech", score: 50 },
+  { id: 7, name: "Salma", age: 40, city: "Rabat", score: 90 },
+  { id: 8, name: "Hanae", age: 21, city: "Marrakech", score: 41 },
+  { id: 9, name: "Ibrahim", age: 27, city: "Casablanca", score: 55 },
+  { id: 10, name: "Jade", age: 24, city: "Agadir", score: 48 }
+];
